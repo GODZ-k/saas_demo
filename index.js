0447bc7c8ae233af3626c0b51c2c8561e9676457
Tanmay/K and A Tech - MERN <tanmaykandatech-mern@192.168.1.9>
2024-10-23 10:31:57 +0530

    main

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import app from "./src/app.js";
 
 
 app.listen(process.env.PORT,()=>{
-    console.log("Server is listning on port 3000")
+    console.log(`Server is listning on port ${process.env.PORT} `)
 })
